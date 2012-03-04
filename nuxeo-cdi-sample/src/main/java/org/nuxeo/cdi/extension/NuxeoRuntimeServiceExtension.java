5cb4621db4ddd914c4df1dc1cc359673fe0ca8d6
Anahide Tchertchian <at@nuxeo.com>
2012-03-04 16:32:45 +0100

    better layouts demo: use sample document model + make the dublincore layout quite work by adding missing widget templates

--- nuxeo-cdi-sample/src/main/java/org/nuxeo/cdi/extension/NuxeoRuntimeServiceExtension.java
+++ nuxeo-cdi-sample/src/main/java/org/nuxeo/cdi/extension/NuxeoRuntimeServiceExtension.java
@@ -8,7 +8,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.enterprise.context.Dependent;
-import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Any;
@@ -19,7 +18,6 @@ import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.enterprise.inject.spi.InjectionTarget;
 import javax.enterprise.util.AnnotationLiteral;
 
 import org.apache.commons.logging.Log;
@@ -30,20 +28,22 @@ import org.nuxeo.runtime.model.RegistrationInfo;
 public class NuxeoRuntimeServiceExtension implements Extension {
 
     protected static Log log = LogFactory.getLog(NuxeoRuntimeServiceExtension.class);
-    
-    void afterBeanDiscovery(@Observes AfterBeanDiscovery abd, BeanManager bm) {
-        
+
+    void afterBeanDiscovery(@Observes
+    AfterBeanDiscovery abd, BeanManager bm) {
+
         log.error("Registering Nuxeo Runtime Services in CDI");
-        
+
         Collection<RegistrationInfo> registrations = Framework.getRuntime().getComponentManager().getRegistrations();
 
         for (RegistrationInfo ri : registrations) {
-            
+
             if (ri.getProvidedServiceNames() != null) {
                 for (String serviceClassName : ri.getProvidedServiceNames()) {
                     try {
                         Class<?> serviceClass = Class.forName(serviceClassName);
-                        log.error("  - Registering Service " + serviceClassName);
+                        // log.error("  - Registering Service " +
+                        // serviceClassName);
                         registerNuxeoRuntimeService(serviceClass, abd, bm);
                     } catch (ClassNotFoundException e) {
                         // TODO Auto-generated catch block
@@ -51,17 +51,19 @@ public class NuxeoRuntimeServiceExtension implements Extension {
                     }
                 }
             }
-            
+
         }
     }
-        
-    protected <T> void  registerNuxeoRuntimeService(final Class<T> nuxeoServiceClass, AfterBeanDiscovery abd, BeanManager bm) {
-        
-        AnnotatedType<T> at = bm.createAnnotatedType(nuxeoServiceClass); 
-                        
-        //final InjectionTarget<T> it = bm.createInjectionTarget(at); 
-        
-        abd.addBean( new Bean<T>() {
+
+    protected <T> void registerNuxeoRuntimeService(
+            final Class<T> nuxeoServiceClass, AfterBeanDiscovery abd,
+            BeanManager bm) {
+
+        AnnotatedType<T> at = bm.createAnnotatedType(nuxeoServiceClass);
+
+        // final InjectionTarget<T> it = bm.createInjectionTarget(at);
+
+        abd.addBean(new Bean<T>() {
 
             @Override
             public T create(CreationalContext<T> ctx) {
@@ -81,15 +83,17 @@ public class NuxeoRuntimeServiceExtension implements Extension {
             @Override
             public Set<Annotation> getQualifiers() {
                 Set<Annotation> qualifiers = new HashSet<Annotation>();
-                qualifiers.add( new AnnotationLiteral<Default>() {} );
-                qualifiers.add( new AnnotationLiteral<Any>() {} );
+                qualifiers.add(new AnnotationLiteral<Default>() {
+                });
+                qualifiers.add(new AnnotationLiteral<Any>() {
+                });
                 return qualifiers;
             }
 
             @Override
             public Class<? extends Annotation> getScope() {
                 return Dependent.class;
-                //return RequestScoped.class;
+                // return RequestScoped.class;
             }
 
             @Override
@@ -121,11 +125,11 @@ public class NuxeoRuntimeServiceExtension implements Extension {
             }
 
             @Override
-            public Set<InjectionPoint> getInjectionPoints() {                
+            public Set<InjectionPoint> getInjectionPoints() {
                 return Collections.emptySet();
             }
-            
+
         });
-        
+
     }
 }
--- nuxeo-cdi-sample/src/main/java/org/nuxeo/cdi/layouts/LayoutBinding.java
+++ nuxeo-cdi-sample/src/main/java/org/nuxeo/cdi/layouts/LayoutBinding.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelFactory;
+
 @Named
 @SessionScoped
 public class LayoutBinding implements Serializable {
@@ -15,9 +18,12 @@ public class LayoutBinding implements Serializable {
 
     protected String description;
 
+    protected DocumentModel sampleDocumentModel;
+
     public LayoutBinding() {
         title = "My title here";
         description = "My description here";
+        sampleDocumentModel = DocumentModelFactory.createDocumentModel("File");
     }
 
     public String getTitle() {
@@ -34,6 +40,14 @@ public class LayoutBinding implements Serializable {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public DocumentModel getSampleDocumentModel() {
+        return sampleDocumentModel;
+    }
+
+    public void setSampleDocumentModel(DocumentModel sampleDocumentModel) {
+        this.sampleDocumentModel = sampleDocumentModel;
     }
 
 }
